490a93fd243e76e96d5e5ab6a965054c75dadf62
Ali <ali.rac200@gmail.com>
2016-11-30 10:29:15 +0500

    Added and passing tests for parsing git2.diff

--- diffparser/src/test/java/org/wickedsource/diffparser/unified/GitDiffTest.java
+++ diffparser/src/test/java/org/wickedsource/diffparser/unified/GitDiffTest.java
@@ -20,7 +20,7 @@ public class GitDiffTest
     private final Logger log = Logger.getLogger( this.toString() );
 
     @Test
-    public void testParse1() throws Exception
+    public void testParse() throws Exception
     {
         // given
         DiffParser parser = new UnifiedDiffParser();
@@ -42,10 +42,7 @@ public class GitDiffTest
         assertEquals( headerLines.toString(), 2, headerLines.size());
 
         Hunk hunk1 = diff.getHunks().get(0);
-        assertEquals(30, hunk1.getFromFileRange().getLineStart());
-        assertEquals(9, hunk1.getFromFileRange().getLineCount());
-        assertEquals(30, hunk1.getToFileRange().getLineStart());
-        assertEquals(9, hunk1.getToFileRange().getLineCount());
+        validateHunk(hunk1, 30, 9, 30, 9);
 
         List<Line> lines = hunk1.getLines();
         assertEquals(10, lines.size());
@@ -65,9 +62,8 @@ public class GitDiffTest
     }
 
     @Test
-    public void testParse2() throws Exception
+    public void testDiff2Parse() throws Exception
     {
-
         // given
         DiffParser parser = new UnifiedDiffParser();
         InputStream in = getClass().getResourceAsStream("git2.diff");
@@ -75,6 +71,55 @@ public class GitDiffTest
         // when
         List<Diff> diffs = parser.parse(in);
 
-        log.info("Diffs: " + diffs.toString());
+        // then
+        assertNotNull(diffs);
+        assertEquals(1, diffs.size());
+
+        Diff diff = diffs.get(0);
+        assertEquals("a/diffparser/src/main/java/org/wickedsource/diffparser/unified/ParserState.java", diff.getFromFileName());
+        assertEquals("b/diffparser/src/main/java/org/wickedsource/diffparser/unified/ParserState.java", diff.getToFileName());
+        assertEquals(3, diff.getHunks().size());
+
+        List<String> headerLines = diff.getHeaderLines();
+        assertEquals( headerLines.toString(), 2, headerLines.size());
+
+        Hunk hunk1 = diff.getHunks().get(0);
+        Hunk hunk2 = diff.getHunks().get(1);
+        Hunk hunk3 = diff.getHunks().get(2);
+
+        validateHunk(hunk1, 15, 6, 15, 7);
+        validateHunk(hunk2, 88, 11, 89, 13 );
+        validateHunk(hunk3, 252, 7, 255, 7 );
+
+        /* Test Hunk 1 lines: */
+        List<Line> lines = hunk1.getLines();
+        assertEquals(7, lines.size());
+        assertEquals(NEUTRAL, lines.get(1).getLineType());
+        assertEquals(TO, lines.get(3).getLineType());
+        assertEquals(NEUTRAL, lines.get(4).getLineType());
+
+        /* Test Hunk 2 lines: */
+        lines = hunk2.getLines();
+        assertEquals(14, lines.size());
+        assertEquals(TO, lines.get(3).getLineType());
+        assertEquals(TO, lines.get(4).getLineType());
+        assertEquals(FROM, lines.get(9).getLineType());
+        assertEquals(TO, lines.get(10).getLineType());
+        assertEquals(NEUTRAL, lines.get(5).getLineType());
+
+        /* Test Hunk 3 lines: */
+        lines = hunk3.getLines();
+        assertEquals(8, lines.size());
+        assertEquals(FROM, lines.get(3).getLineType());
+        assertEquals(TO, lines.get(4).getLineType());
+        assertEquals(NEUTRAL, lines.get(5).getLineType());
+    }
+
+    private void validateHunk(Hunk hunk, int fromLineStart, int fromLineCount, int toLineStart, int toLineCount)
+    {
+        assertEquals(fromLineStart, hunk.getFromFileRange().getLineStart());
+        assertEquals(fromLineCount, hunk.getFromFileRange().getLineCount());
+        assertEquals(toLineStart, hunk.getToFileRange().getLineStart());
+        assertEquals(toLineCount, hunk.getToFileRange().getLineCount());
     }
 }
